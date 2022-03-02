d3c0b05940ddc1bd2a70d1f3c6136912e7e4e2bd
TakitoLoko <90416619+TakitoLoko@users.noreply.github.com>
2022-03-02 12:53:21 -0700

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCmSX9JsM4j5RQ5b9DBUKJBaI3w_so9G3g",
+
+    authDomain: "java-project-50df9.firebaseapp.com",
+
+    databaseURL: "https://java-project-50df9-default-rtdb.firebaseio.com",
+
+    projectId: "java-project-50df9",
+
+    storageBucket: "java-project-50df9.appspot.com",
+
+    messagingSenderId: "596563663396",
+
+    appId: "1:596563663396:web:42879338e7548d5e3096b1"
+
+  };
+
+
+  // Initialize Firebase
+
+// const app = initializeApp(firebaseConfig);
