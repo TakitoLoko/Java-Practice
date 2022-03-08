29f91ca2fb2f72a9392bbd09c489b12dcafcefff
TakitoLoko <90416619+TakitoLoko@users.noreply.github.com>
2022-03-08 02:15:13 -0700

    Fixed pictures and added more fields to resume builder

--- sub-html-pages/resume.js
+++ sub-html-pages/resume.js
@@ -1,7 +1,22 @@
 window.addEventListener('load', () => {
     const params = (new URL(document.location)).searchParams
     const name = params.get('name');
+    const address = params.get('address');
+    const number = params.get('number');
+    const email= params.get('email');
+    const languages = params.get('languages');
+    const software = params.get('software');
+    const hardware= params.get('hardware');
+
     
     document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-address').innerHTML = address;
+    document.getElementById('result-number').innerHTML = number;
+    document.getElementById('result-email').innerHTML = email;
+    document.getElementById('result-languages').innerHTML = languages;
+    document.getElementById('result-software').innerHTML = software;
+    document.getElementById('result-hardware').innerHTML = hardware;
+
+
     
 })
