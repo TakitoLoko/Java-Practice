961ca3b7534025b3179247cf9bff140d34b295ea
TakitoLoko <90416619+TakitoLoko@users.noreply.github.com>
2022-03-08 00:13:35 -0700

    fixed css

--- sub-html-pages/resume.js
+++ sub-html-pages/resume.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     const params = (new URL(document.location)).searchParams
-    const name = params.get('name')
+    const name = params.get('name');
     
     document.getElementById('result-name').innerHTML = name;
     
